3114e88f97d9e0d602649a9081ee58e4d9a8ac70
Jean Redfern <jpredfer@iu.edu>
2022-02-03 15:40:06 -0500

    create project

---
+++ hello.js
@@ -0,0 +1,13 @@
+console.log("Hello World!");
+
+var readline = require('readline');
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Enter Your Name: ', function(answer){
+  console.log('Hello ' +  answer);
+  rl.close();
+});
